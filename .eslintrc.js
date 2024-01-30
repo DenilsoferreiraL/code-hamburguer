4dff29f387f2e279cb080c64911e0803199f14bc
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-01-30 11:28:06 -0300

    chore: routes and server configured with eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    camelcase: 'off',
+    quotes: ['error', 'single','semi'],
+  },
+};
