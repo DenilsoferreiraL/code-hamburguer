27fb1cd7ec4364347b5e907d33733e71a8b7158c
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-02-15 17:04:12 -0300

    chore: password hash created for encryption

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,11 +4,10 @@ import * as Yup from 'yup'
 
 class UserController {
 	async store(request, response) {
-    
 		const schema = Yup.object().shape({
 			name: Yup.string().required(),
 			email: Yup.string().email().required(),
-			password_hash: Yup.string().required().min(6),
+			password: Yup.string().required().min(6),
 			admin: Yup.boolean()
 		})
 
@@ -18,16 +17,24 @@ class UserController {
 			return response.status(400).json({ error: err.errors })
 		}
 
-		const { name, email, password_hash, admin } = request.body
+		const { name, email, password, admin } = request.body
+
+		const userExist = await User.findOne({
+			where: { email }
+		})
+
+		if (userExist) {
+			return response.status(400).json({ error: 'usere already exists' })
+		}
 
 		const user = await User.create({
 			id: v4(),
 			name,
 			email,
-			password_hash,
+			password,
 			admin
 		})
-		return response.json(user)
+		return response.status(201).json({ id: user.id, name, email, admin })
 	}
 }
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,20 +1,27 @@
 import Sequelize, { Model } from 'sequelize'
-
+import bcrypt from 'bcrypt'
 class User extends Model {
-  static init (sequelize) {
-    super.init(
-      // Primeiro parâmetro
-      {
-        name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password_hash: Sequelize.STRING,
-        admin: Sequelize.BOOLEAN
-      },
-      {
-        // Segundo parâmetro
-        sequelize
-      }
-    )
-  }
+	static init(sequelize) {
+		super.init(
+			// Primeiro parâmetro
+			{
+				name: Sequelize.STRING,
+				email: Sequelize.STRING,
+				password: Sequelize.VIRTUAL,
+				password_hash: Sequelize.STRING,
+				admin: Sequelize.BOOLEAN
+			},
+			{
+				// Segundo parâmetro
+				sequelize
+			}
+		)
+		this.addHook('beforeSave', async user => {
+			if (user.password) {
+				user.password_hash = await bcrypt.hash(user.password, 10)
+			}
+		})
+		return this
+	}
 }
 export default User
