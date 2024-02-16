c86bbd84c00212e3c9d47e32a14f709bf4aa3187
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-02-16 09:25:48 -0300

    chore: login validation with Yup

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup'
+import User from '../models/User'
+
+class SessionController {
+	async store(request, response) {
+		const schema = Yup.object().shape({
+			email: Yup.string().email().required(),
+			password: Yup.string().required()
+		})
+
+		const incorrectUserEmailOrPassword = () => {
+			return response
+				.status(400)
+				.json({ error: 'User not found or password incorrect' })
+		}
+
+		const { email, password } = request.body
+
+		if (!(await schema.isValid(request.body))) incorrectUserEmailOrPassword()
+
+		const user = await User.findOne({
+			where: { email }
+		})
+
+		if (!user || !(await user.checkPassword(password)))
+			incorrectUserEmailOrPassword()
+
+		return response.json({
+			id: user.id,
+			email,
+			name: user.name,
+			admin: user.admin
+		})
+	}
+}
+
+export default new SessionController()
