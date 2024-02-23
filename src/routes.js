fc944c12c5d7f78188562f141c899645c30dd222
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-02-23 17:04:21 -0300

    chore: category table implemented with validation.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import ProductController from './app/controllers/ProductController'
 import multer from 'multer'
 import multerConfig from './config/multer'
 import authMiddleware from './app/middlewares/auth'
+import CategoryController from './app/controllers/CategoryController'
 
 const uploads = multer(multerConfig)
 
@@ -17,7 +18,9 @@ routes.post('/sessions', SessionController.store)
 routes.use(authMiddleware)
 
 routes.post('/products', uploads.single('file'), ProductController.store)
-
 routes.get('/products', ProductController.index)
+
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 export default routes
