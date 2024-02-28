9f32d45e251cd23b35f2bffd914e8c16926b1c5a
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-02-28 10:31:56 -0300

    chore: Connection created with mongo.

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,13 @@ import configDataBase from '../config/database'
 import User from '../app/models/User'
 import Product from '../app/models/Product'
 import Category from '../app/models/Category'
+import mongoose from 'mongoose'
 
 const models = [User, Product, Category]
 class Database {
 	constructor() {
 		this.init()
+		this.mongo()
 	}
 
 	init() {
@@ -15,6 +17,11 @@ class Database {
 		models
 			.map(model => model.init(this.connection))
 			.map(model => model.associate && model.associate(this.connection.models))
+	}
+	mongo() {
+		this.mongoConnection = mongoose.connect(
+			'mongodb://localhost:27017/codeburger'
+		)
 	}
 }
 
