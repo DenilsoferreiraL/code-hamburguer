3feb5c350752c10c636a3e9b251c35edf98b7283
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-03-01 14:28:56 -0300

    chore:  Created by updating the offers column and adding an image via the url.created by updating the offers column and adding an image via the url.

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -22,6 +22,8 @@ class CategoryController {
 
 		const { name } = request.body
 
+		const { filename: path } = request.file
+
 		//Checks the category if it already exists in the database, if it does, it returns an error otherwise, it creates the category and returns its name and ID.
 		const categoryExist = await Category.findOne({
 			where: {
@@ -33,7 +35,7 @@ class CategoryController {
 			return response.status(400).json({ error: 'Category already created' })
 		}
 
-		const { id } = await Category.create({ name })
+		const { id } = await Category.create({ name, path })
 
 		return response.json({ name, id })
 	}
@@ -42,6 +44,49 @@ class CategoryController {
 		const category = await Category.findAll()
 
 		return response.json(category)
+	}
+
+	async update(request, response) {
+		try {
+			const schema = Yup.object().shape({
+				name: Yup.string()
+			})
+
+			try {
+				await schema.validateSync(request.body, { abortEarly: false })
+			} catch (err) {
+				return response.status(400).json({ error: err.errors })
+			}
+
+			const { admin: isAdmin } = await User.findByPk(request.userId)
+
+			if (!isAdmin) {
+				return response.status(401).json()
+			}
+
+			const { name } = request.body
+
+			const { id } = request.params
+
+			const category = await Category.findByPk(id)
+
+			if (!category) {
+				return response
+					.status(401)
+					.json({ error: 'Make sure your category ID is correct' })
+			}
+
+			let path
+			if (request.file) {
+				path = request.file.filename
+
+				await Category.update({ name, path }, { where: { id } })
+
+				return response.status(200).json()
+			}
+		} catch (err) {
+			console.log(err)
+		}
 	}
 }
 export default new CategoryController()
