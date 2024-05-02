2e4e4b8287a9a6d52308059092e4b4ec1c3d856b
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-05-02 16:06:10 -0300

    Chore (app): added configuration of CORS options to allow access to the API from the 'https://code-burger-react-azure.vercel.app/' domain. Credentials are also allowed, ensuring that requests with credentials (such as cookies or authorization headers) are not allowed.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import { resolve } from 'path';
 import cors from 'cors';
 import './database';
 
+const corsOptions = {
+  origin: 'https://code-burger-react-azure.vercel.app/',
+  Credentials: true,
+}
+
 class App {
   constructor() {
     this.app = express();
-    this.app.use(cors());
+    this.app.use(cors(corsOptions));
 
     this.middlewares();
     this.routes();
